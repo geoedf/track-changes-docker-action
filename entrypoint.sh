87e934e859271d6fd541d50f0d8fffe3a9827d95
Rajesh Kalyanam <rkalyanapurdue@gmail.com>
2020-06-01 18:24:05 -0400

    updated script to return list of folders

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,18 @@
 
 cd /github/workspace 
 
-echo `git diff-tree --no-commit-id --name-only -r $GITHUB_SHA`
+changed_files=$(git diff-tree --no-commit-id --name-only -r $GITHUB_SHA)
+
+indx=0
+declare -a res
+
+for path in $changed_files
+do
+  if [ `basename $path` != $path ]
+  then
+      val=$(echo $path | tr "/" "\n" | head -1)
+      res[indx]=$val
+      ((indx++))
+  fi
+done
+echo "::set-output name=folders::${res[*]}"
